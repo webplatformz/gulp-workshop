3a1813837cb49bba5289599a7c3cbe80628e47e8
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2015-03-31 01:02:07 +0200

    Minor js lint and uglify fixes.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-var app = angular.module("myApp", []);
+var myApp = angular.module("myApp", []);
--- public/controller/myCtrl.js
+++ public/controller/myCtrl.js
@@ -1,5 +1,7 @@
-app.controller("myCtrl", function($scope) {
+'use strict';
+
+myApp.controller("myCtrl",['$scope',function($scope) {
     $scope.message = "";
     $scope.left  = function() {return 200 - $scope.message.length;};
     $scope.clear = function() {$scope.message = "";};
-});
+}]);
